8d4d3f18bebc894293e93a3eeca9a388301896fa
sukhijab <sukhijab@student.ethz.ch>
2023-08-01 16:28:35 +0200

    minor fix in base optimizer

--- mbpo/optimizers/base_optimizer.py
+++ mbpo/optimizers/base_optimizer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Generic
+from typing import Generic, Tuple
 from mbpo.utils.type_aliases import OptimizerState
 import chex
 from mbpo.systems.base_systems import System, SystemParams
@@ -11,8 +11,8 @@ class BaseOptimizer(ABC, Generic[OptimizerState]):
         pass
 
     @abstractmethod
-    def act(self, obs: chex.Array, opt_state: OptimizerState, system_params: SystemParams) -> [chex.Array,
-                                                                                               OptimizerState]:
+    def act(self, obs: chex.Array, opt_state: OptimizerState, system_params: SystemParams) ->\
+            Tuple[chex.Array, OptimizerState]:
         pass
 
     def train(self, opt_state: OptimizerState, *args, **kwargs) -> OptimizerState:
