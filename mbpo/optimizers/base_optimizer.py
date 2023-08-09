d432af122d505338bd63d40707b7771d48af6657
sukhijab <sukhijab@student.ethz.ch>
2023-08-09 13:16:43 +0200

    base optimizer api

--- mbpo/optimizers/base_optimizer.py
+++ mbpo/optimizers/base_optimizer.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Generic, Tuple
-from mbpo.utils.type_aliases import OptimizerState
+from mbpo.utils.type_aliases import OptimizerState, OptimizerTrainingOutPut
 import chex
 from mbpo.systems.base_systems import System, SystemParams
 
@@ -11,9 +11,9 @@ class BaseOptimizer(ABC, Generic[OptimizerState]):
         pass
 
     @abstractmethod
-    def act(self, obs: chex.Array, opt_state: OptimizerState, system_params: SystemParams) ->\
+    def act(self, obs: chex.Array, opt_state: OptimizerState, system_params: SystemParams, evaluate: bool = True) ->\
             Tuple[chex.Array, OptimizerState]:
         pass
 
-    def train(self, opt_state: OptimizerState, *args, **kwargs) -> OptimizerState:
+    def train(self, opt_state: OptimizerState, *args, **kwargs) -> OptimizerTrainingOutPut:
         return opt_state
