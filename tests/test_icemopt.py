4495c1550cc7241268389d6fb17641b4626bd586
sukhijab <sukhijab@student.ethz.ch>
2023-11-01 12:58:40 +0100

    updated test for icemopt

--- tests/test_icemopt.py
+++ tests/test_icemopt.py
@@ -4,24 +4,24 @@ import jax
 import jax.numpy as jnp
 
 key = jax.random.PRNGKey(0)
-optimizer_key, key = jax.random.split(key, 2)
+optimizer_key, init_key, key = jax.random.split(key, 3)
 system = PendulumSystem()
 system_state = system.reset(key)
 system_params = system_state.system_params
 opt_params = iCemParams()
-cem_optimizer = iCemTO(horizon=20, action_dim=1, system=system, opt_params=opt_params, key=optimizer_key)
-
-cem_optimizer_state = cem_optimizer.initialize_optimizer()
+cem_optimizer = iCemTO(horizon=20, action_dim=1, system=None, opt_params=opt_params, key=optimizer_key)
+cem_optimizer.set_system(system)
+cem_optimizer_state = cem_optimizer.init(init_key)
 
 horizon = 200
 
 
 def rollout_cem(carry, ins):
     system_state, cem_optimizer_state = carry[0], carry[1]
-    action, new_cem_optimizer_state = cem_optimizer.act(obs=system_state.x_next, opt_state=cem_optimizer_state,
-                                                        system_params=system_state.system_params)
+    action, new_cem_optimizer_state = cem_optimizer.act(obs=system_state.x_next, opt_state=cem_optimizer_state)
     new_system_state = system.step(x=system_state.x_next, u=action,
                                    system_params=system_state.system_params)
+    new_cem_optimizer_state = new_cem_optimizer_state.replace(system_params=new_system_state.system_params)
 
     carry = [new_system_state, new_cem_optimizer_state]
     outs = [new_system_state.x_next, new_system_state.reward]
