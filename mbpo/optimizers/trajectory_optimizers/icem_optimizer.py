6d03e1d29f9ea266d8017321ddfa9f42899763f5
Klemens Iten <kiten@ethz.ch>
2024-10-31 16:44:07 +0100

    new iCEM optimizer

--- mbpo/optimizers/trajectory_optimizers/icem_optimizer.py
+++ mbpo/optimizers/trajectory_optimizers/icem_optimizer.py
@@ -1,18 +1,23 @@
 """Generate colored noise. Taken from: https://github.com/felixpatzelt/colorednoise/blob/master/colorednoise.py"""
-import chex
-import numpy as np
-from jax.numpy import sqrt, newaxis
-from jax.numpy.fft import irfft, rfftfreq
-import jax
-import jax.numpy as jnp
+from abc import abstractmethod
 from functools import partial
 from typing import NamedTuple, Generic
-from mbpo.utils.optimizer_utils import rollout_actions
-from mbpo.systems.base_systems import SystemParams
+
+import chex
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import numpy as np
+from jax import vmap
+from jax.nn import relu
+from jax.numpy import sqrt, newaxis
+from jax.numpy.fft import irfft, rfftfreq
+from jaxtyping import Float, Array, Key, Scalar
 from mbpo.optimizers.base_optimizer import BaseOptimizer
-from mbpo.utils.type_aliases import OptimizerState, OptimizerTrainingOutPut
-from mbpo.systems.rewards.base_rewards import RewardParams
 from mbpo.systems.dynamics.base_dynamics import DynamicsParams
+from mbpo.systems.rewards.base_rewards import RewardParams
+from mbpo.utils.optimizer_utils import rollout_actions
+from mbpo.utils.type_aliases import OptimizerState
 
 
 @partial(jax.jit, static_argnums=(0, 1, 3))
@@ -143,14 +148,18 @@ def powerlaw_psd_gaussian(exponent: float, size: int, rng: jax.random.PRNGKey, f
 
 class iCemParams(NamedTuple):
     """
-    maxiter: maximum iterations.
-    grad_norm_threshold: tolerance for stopping optimization.
-    make_psd: whether to zero negative eigenvalues after quadratization.
-    psd_delta: The delta value to make the problem PSD. Specifically, it will
-        ensure that d^2c/dx^2 and d^2c/du^2, i.e. the hessian of cost function
-        with respect to state and control are always positive definite.
-    alpha_0: initial line search value.
-    alpha_min: minimum line search value.
+    num_particles: int = 10
+    num_samples: int = 500
+    num_elites: int = 50
+    init_std: float = initial std of the samples
+    alpha: float = how softly we update the mean and var of elites to the next iteration
+    num_steps: int = how many steps of samping we do
+    exponent: float = How colored noise we want - higher the more correlated are the samples
+    elite_set_fraction: float = Share of elites we take to the next iteration
+    u_min: float | chex.Array = minimal value for action
+    u_max: float | chex.Array = maximal value for action
+    warm_start: bool = If we shift the action sequence for one and repeat the last action at initialization
+
     """
     num_particles: int = 10
     num_samples: int = 500
@@ -163,6 +172,16 @@ class iCemParams(NamedTuple):
     u_min: float | chex.Array = -1.0
     u_max: float | chex.Array = 1.0
     warm_start: bool = True
+    lambda_constraint: float = 1e4
+
+
+class ICemCarry(NamedTuple):
+    key: Key[Array, '2']
+    mean: Float[Array, 'horizon action_dim']
+    std: Float[Array, 'horizon action_dim']
+    best_value: Scalar
+    best_sequence: Float[Array, 'horizon action_dim']
+    prev_elites: Float[Array, 'num_elites horizon action_dim']
 
 
 @chex.dataclass
@@ -175,12 +194,30 @@ class iCemOptimizerState(OptimizerState, Generic[DynamicsParams, RewardParams]):
         return self.best_sequence[0]
 
 
+class AbstractCost:
+
+    def __init__(self, horizon: int):
+        self.horizon = horizon
+
+    @abstractmethod
+    def __call__(self,
+                 states: Float[Array, 'horizon observation_dim'],
+                 actions: Float[Array, 'horizon action_dim'],
+                 ) -> Scalar:
+        # This return the cost of the trajectory, i.e., \sum_{t=0}^{T-1}c(\vx_t, \vu_t)
+        # We want the cost to be \E\left[\sum_{t=0}^{T-1}c(\vx_t, \vu_t)\right] <= 0
+        pass
+
+
 class iCemTO(BaseOptimizer, Generic[DynamicsParams, RewardParams]):
     def __init__(self,
                  horizon: int,
                  action_dim: int,
-                 key: jax.random.PRNGKey = jax.random.PRNGKey(0),
+                 key: chex.PRNGKey = jax.random.PRNGKey(0),
                  opt_params: iCemParams = iCemParams(),
+                 cost_fn: AbstractCost | None = None,
+                 use_optimism: bool = True,
+                 use_pessimism: bool = True,
                  *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
@@ -188,8 +225,19 @@ class iCemTO(BaseOptimizer, Generic[DynamicsParams, RewardParams]):
         self.opt_params = opt_params
         self.key = key
         self.opt_dim = (horizon,) + (action_dim,)
+        self.action_dim = action_dim
+        self.horizon = horizon
+        self.cost_fn = cost_fn
+        if use_optimism:
+            self.summarize_raw_samples = jnp.max
+        else:
+            self.summarize_raw_samples = jnp.mean
+        if use_pessimism:
+            self.summarize_cost_samples = jnp.max
+        else:
+            self.summarize_cost_samples = jnp.mean
 
-    def init(self, key: chex.Array):
+    def init(self, key: chex.Array) -> iCemOptimizerState:
         assert self.system is not None, "iCem optimizer requires system to be defined."
         init_key, dummy_buffer_key, key = jax.random.split(key, 3)
         system_params = self.system.init_params(init_key)
@@ -205,87 +253,124 @@ class iCemTO(BaseOptimizer, Generic[DynamicsParams, RewardParams]):
     @partial(jax.jit, static_argnums=0)
     def optimize(
             self,
-            initial_state: chex.Array,
+            initial_state: Float[Array, 'observation_dim'],
             opt_state: iCemOptimizerState,
     ):
         assert self.system is not None, "iCem optimizer requires system to be defined."
-        initial_state = jnp.repeat(jnp.expand_dims(initial_state, 0), self.opt_params.num_particles, 0)
 
-        def objective(seq):
-            optimize_fn = lambda x: rollout_actions(
-                system=self.system,
-                system_params=opt_state.system_params,
-                init_state=x,
-                horizon=self.horizon,
-                actions=seq,
-            )
-            transitions = jax.vmap(optimize_fn)(initial_state)
-            return transitions.reward.mean()
+        # To estimate mean trajectory under some action sequence we sample self.opt_params.num_particles number of
+        # noisy realization of the dynamics propagation
+        def objective(seq: Float[Array, 'horizon action_dim'], key: Key[Array, '2']) -> Scalar:
 
-        get_best_action = lambda best_val, best_seq, val, seq: [val[-1].squeeze(), seq[-1]]
+            def optimize_fn(init_state: Float[Array, 'observation_dim'], rng: Key[Array, '2']):
+                system_params = opt_state.system_params.replace(key=rng)
+                return rollout_actions(system=self.system,
+                                       system_params=system_params,
+                                       init_state=init_state,
+                                       horizon=self.horizon,
+                                       actions=seq,
+                                       )
+
+            particles_rng = jr.split(key, self.opt_params.num_particles)
+            transitions = jax.vmap(optimize_fn, in_axes=(None, 0))(initial_state, particles_rng)
+            cost = 0
+
+            # We summarize cost with mean or max (if optimism is true)
+            reward = self.summarize_raw_samples(jnp.mean(transitions.reward, axis=-1))
+            if self.cost_fn is not None:
+                cost = vmap(self.cost_fn)(transitions.observation, transitions.action)
+                assert cost.shape == (self.opt_params.num_particles,)
+                # We summarize cost with mean or max (if pessimism is true)
+                cost = self.summarize_cost_samples(cost)
+            return reward - self.opt_params.lambda_constraint * relu(cost)
+
+        get_best_action = lambda best_val, best_seq, val, seq: [val[-1], seq[-1]]
         get_curr_best_action = lambda best_val, best_seq, val, seq: [best_val, best_seq]
         num_prev_elites_per_iter = max(int(self.opt_params.elite_set_fraction * self.opt_params.num_elites), 1)
 
-        def step(carry, ins):
-            key = carry[0]
-            mu = carry[1]
-            sig = carry[2]
-            best_val = carry[3]
-            best_seq = carry[4]
-            prev_elites = carry[5]
-            mu = mu.reshape(-1)
-            sig = sig.reshape(-1)
-            sampling_rng = jax.random.split(key=key, num=self.opt_params.num_samples + 1)
-            key = sampling_rng[0]
-            sampling_rng = sampling_rng[1:]
-            opt_size = self.opt_dim[0] * self.opt_dim[1]
-            colored_samples = jax.vmap(
-                lambda rng: powerlaw_psd_gaussian(exponent=self.opt_params.exponent, size=opt_size, rng=rng))(
-                sampling_rng)
-            action_samples = mu + colored_samples * sig
+        def step(carry: ICemCarry, ins):
+            # Split the key
+            sampling_rng, particles_rng = jax.random.split(carry.key)
+            sampling_rng = jax.random.split(key=sampling_rng, num=self.opt_params.num_samples + 1)
+            key, sampling_rng = sampling_rng[0], sampling_rng[1:]
+            particles_rng = jr.split(particles_rng, self.opt_params.num_samples + num_prev_elites_per_iter)
+
+            # We create colored samples from gaussian of size (num_samples, np.prod(self.opt_dim))
+            sampling_rng = vmap(lambda x: jr.split(x, self.action_dim))(sampling_rng)
+
+            def colored_sample_fn(rng):
+                return powerlaw_psd_gaussian(exponent=self.opt_params.exponent, size=self.horizon, rng=rng)
+
+            colored_samples_all_dims_fn = vmap(colored_sample_fn, out_axes=1)
+            colored_samples = vmap(colored_samples_all_dims_fn)(sampling_rng)
+            assert colored_samples.shape == (self.opt_params.num_samples, self.horizon, self.action_dim)
+
+            # Add noise, clip to [u_min, u_max], and reshape back
+            action_samples = carry.mean + colored_samples * carry.std
             action_samples = jnp.clip(action_samples, a_max=self.opt_params.u_max, a_min=self.opt_params.u_min)
-            action_samples = action_samples.reshape((-1,) + self.opt_dim)
             action_samples = jnp.concatenate([action_samples, prev_elites], axis=0)
-            values = jax.vmap(objective)(action_samples)
-            best_elite_idx = np.argsort(values, axis=0).squeeze()[-self.opt_params.num_elites:]
+
+            # Calculate objective for all the samples
+            values = jax.vmap(objective)(action_samples, particles_rng)
+            assert values.shape == (self.opt_params.num_samples + num_prev_elites_per_iter,)
+
+            # Prepare indices of elite samples (i.e. samples with the highest reward)
+            best_elite_idx = np.argsort(values, axis=0)[-self.opt_params.num_elites:]
+
+            # Take elite actions and their values
             elites = action_samples[best_elite_idx]
             elite_values = values[best_elite_idx]
+
+            # Compute mean and var of elites actions
             elite_mean = jnp.mean(elites, axis=0)
             elite_var = jnp.var(elites, axis=0)
-            mean = mu.reshape(self.opt_dim) * self.opt_params.alpha + (1 - self.opt_params.alpha) * elite_mean
-            var = jnp.square(sig.reshape(self.opt_dim)) * self.opt_params.alpha + (
-                    1 - self.opt_params.alpha) * elite_var
+
+            # Do soft update of the mean and var
+            mean = carry.mean * self.opt_params.alpha + (1 - self.opt_params.alpha) * elite_mean
+            var = jnp.square(carry.std) * self.opt_params.alpha + (1 - self.opt_params.alpha) * elite_var
+
+            # Compute std of the soft updated elites actions
             std = jnp.sqrt(var)
-            best_elite = elite_values[-1].squeeze()
-            bests = jax.lax.cond(best_val <= best_elite,
+
+            # Find the best action so far
+            best_elite = elite_values[-1]
+            bests = jax.lax.cond(carry.best_value <= best_elite,
                                  get_best_action,
                                  get_curr_best_action,
-                                 best_val,
-                                 best_seq,
+                                 carry.best_value,
+                                 carry.best_sequence,
                                  elite_values,
                                  elites)
-            best_val = bests[0]
-            best_seq = bests[-1]
+            best_val, best_seq = bests[0], bests[-1]
             outs = [best_val, best_seq]
-            elite_set = jnp.atleast_2d(elites[-num_prev_elites_per_iter:]).reshape((-1,) + self.opt_dim)
-            carry = [key, mean, std, best_val, best_seq, elite_set]
+
+            # Take only num_prev_elites_per_iter elites to the next iteration
+            elite_set = elites[-num_prev_elites_per_iter:]
+
+            carry = ICemCarry(key=key, mean=mean, std=std, best_value=best_val, best_sequence=best_seq,
+                              prev_elites=elite_set)
             return carry, outs
 
         best_value = -jnp.inf
         mean = jnp.zeros(self.opt_dim)
+
+        # If we warm start the optimization we shift the action sequence for one and repeat the last action
         if self.opt_params.warm_start:
             mean = mean.at[:-1].set(opt_state.best_sequence[1:])
             mean = mean.at[-1].set(opt_state.best_sequence[-1])
+
         std = jnp.ones(self.opt_dim) * self.opt_params.init_std
         best_sequence = mean
         prev_elites = jnp.zeros((num_prev_elites_per_iter,) + self.opt_dim)
         optimizer_key, key = jax.random.split(opt_state.key, 2)
         new_opt_state = opt_state.replace(key=key)
-        carry = [optimizer_key, mean, std, best_value, best_sequence, prev_elites]
+        carry = ICemCarry(key=optimizer_key, mean=mean, std=std, best_value=best_value, best_sequence=best_sequence,
+                          prev_elites=prev_elites)
         carry, outs = jax.lax.scan(step, carry, xs=None, length=self.opt_params.num_steps)
         new_opt_state = new_opt_state.replace(best_sequence=outs[1][-1, ...], best_reward=outs[0][-1, ...])
         return new_opt_state
 
+    @partial(jax.jit, static_argnums=0)
     def act(self, obs: chex.Array, opt_state: iCemOptimizerState, evaluate: bool = True):
         new_opt_state = self.optimize(initial_state=obs, opt_state=opt_state)
         return new_opt_state.action, new_opt_state
@@ -310,3 +395,4 @@ if __name__ == "__main__":
         axs[i].plot(x, y, label="coefficient " + str(exponent))
         axs[i].legend()
     plt.show()
+    
